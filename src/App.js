1b8b0581e8b55399399c2d167197d09067127716
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-26 17:18:49 -0300

    Hello world!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+
 import logo from './logo.svg';
 import './App.css';
+// import Hello from './components/Hello';
+// import Counter from './components/Counter';
 
+import React, { Component } from "react";
+
+// Parent Component [Root Component]
+// Stateless component [functional component]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div>
+            <h1>Hello Robinson!</h1>
+
+            
+        </div>
+    );
 }
 
 export default App;
