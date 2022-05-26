17275f8b6375967dc4f7bfc893d6e697f4c7861d
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-26 17:40:11 -0300

    cass based component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Hello from './components/Hello';
 // import Counter from './components/Counter';
 
-import React, { Component } from "react";
+//import React, { Component } from "react";
 
-// Parent Component [Root Component]
+// Parent Component [Root Component] 
 // Stateless component [functional component]
 function App() {
     return ( 
         <div>
-            <Hello name="prop name"/>
+            <Hello name="Rob" />
         </div>
     );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,16 +1,31 @@
-import React, { Component } from "react";
+import React, {
+    Component
+} from "react";
+
+// Secon syntax : class based component
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <h1> Hello { this.props.name } </h1>
+        )
+    }
+}
 
 
+
+
+
+
+
+/*
+functional com props
 function Hello(props){
     return(
         <h1> { props.name }</h1>
     )
-
-
-
 }
-
-
+*/
 
 /*
 
